babbd1883d8d138e0700fa1cb4ac6576639cdf1b
slashformotion <slashformotion@protonmail.com>
2026-03-06 23:31:16 +0100

    chore: remove unused tools from flake

--- shell.nix
+++ shell.nix
@@ -9,13 +9,6 @@ pkgs.mkShell {
     gnumake
     just
 
-    # go development
-    go
-    gopls
-    go-tools
-    delve
-    golangci-lint
-
     # rust development (using stable from overlay)
     (rust-bin.stable.latest.default.override {
       extensions = ["rust-src" "rust-analyzer"];
